515bb0b47e20260b1fd889927d93d851df4d7d73
Davi Passos <you@example.com>
2025-04-17 14:42:35 -0300

    fix: usa process.env.PORT para compatibilidade com Render

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const qrRoutes = require('./routes/qrcode');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'views')));
 
